67f9e0a6d9f3d399de151715d266957e7652bf44
Karan Kumar <karankumarmalhi7755@gmail.com>
2025-09-05 19:25:57 +0500

    adding basic code syntex

---
+++ 01-basic/hello.py
@@ -0,0 +1,68 @@
+# print("Hello World")
+
+# def chai(n) :
+#     print(n)
+
+# chai("Chai pyo Zindgi jyo")
+# tea = {"Masala":"Spaicy", "Ginger": "Zesty", "Green": "Fresh"}
+
+# print(tea)
+
+# for key, value in tea.items():
+#     print(key, value)
+
+# if "Masala" in tea : 
+#     print("I have Masala Chai")
+
+
+# print(len(tea))
+# tea["Earl Gray"] = "Citrus"
+
+# print(tea)
+
+# tea.pop("Masala")
+# print(tea)
+
+# print(tea.popitem())
+# del tea["Green"]
+# print(tea)
+
+# tea_copy = tea.copy()
+
+# print(tea_copy)
+
+tea_Shop = {
+    "chai": {
+        "Masala":"Spicy",
+        "Ginger":"Zesty",
+        "Green":"Fresh"
+    },
+    "Tea":{
+        "Green":"Mild",
+        "Black":"Strong"
+    }
+}
+# print(tea_Shop)
+# print(tea_Shop["chai"])
+
+# print(tea_Shop["chai"]["Ginger"])
+
+squared_num = {x:x**2 for x in range(6)}
+
+# print(squared_num)
+# squared_num.clear()
+# print(squared_num)
+
+# keys = ["Masala", "Ginger", "Lemon"]
+# print(keys)
+# defualt_value = "Delicious"
+# new_dict = dict.fromkeys(keys, 
+# )
+# print(new_dict)
+
+
+
+
+
+
+
